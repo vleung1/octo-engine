4599ecb7d056634791fe05e007936c57d830a52f
Vincent <vleung1@gmail.com>
2018-05-17 09:34:28 -0400

    Test final commit to be merged to master on machineB

--- Git/src/newdev.java
+++ Git/src/newdev.java
@@ -4,8 +4,9 @@ public class newdev {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//from machineB
 		System.out.println("changes meant for new dev branch");
-
+		//some changes from machineA
 	}
 
 }
